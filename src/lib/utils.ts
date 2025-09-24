0f373019870ec2c24c02e4cb209d8436893146cd
Konstantin <konstantin.kim@flexsoft.uz>
2025-09-24 11:35:06 +0900

    feat: add useLocalStorage utility hook with storage value handling functionality

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,27 @@
 import { type ClassValue, clsx } from "clsx";
+import { useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+function getStorageValue<T>(key: string, defaultValue: T): T {
+  const saved = localStorage.getItem(key);
+  if (saved === null) return defaultValue;
+  const initial = JSON.parse(saved);
+  return initial || defaultValue;
+}
+
+export function useLocalStorage<T>(key: string, defaultValue: T) {
+  const [value, setValue] = useState(() => {
+    return getStorageValue(key, defaultValue);
+  });
+
+  useEffect(() => {
+    // storing input name
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
 }
