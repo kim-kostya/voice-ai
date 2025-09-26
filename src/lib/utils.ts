37e5af0a496351d283b313c1d7bab9c5c6cb6941
Konstantin <konstantin.kim@flexsoft.uz>
2025-09-26 23:16:31 +0900

    feat: enhance useLocalStorage with default value initialization and stricter type definitions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
-import { useEffect, useState } from "react";
+import { type Dispatch, type SetStateAction, useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -8,12 +8,18 @@ export function cn(...inputs: ClassValue[]) {
 
 function getStorageValue<T>(key: string, defaultValue: T): T {
   const saved = localStorage.getItem(key);
-  if (saved === null) return defaultValue;
+  if (saved === null) {
+    localStorage.setItem(key, JSON.stringify(defaultValue));
+    return defaultValue;
+  }
   const initial = JSON.parse(saved);
   return initial || defaultValue;
 }
 
-export function useLocalStorage<T>(key: string, defaultValue: T) {
+export function useLocalStorage<T>(
+  key: string,
+  defaultValue: T,
+): [T, Dispatch<SetStateAction<T>>] {
   const [value, setValue] = useState(() => {
     return getStorageValue(key, defaultValue);
   });
