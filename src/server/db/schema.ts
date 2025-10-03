71fa1ec969da26367c222dbe7469e406247cd983
Konstantin <konstantin.kim@flexsoft.uz>
2025-10-03 21:18:55 +0900

    feat: add `users` schema and update `reminders` schema with references and auto-increment

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,14 @@
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+export const users = sqliteTable("users", {
+  id: int("id").primaryKey({ autoIncrement: true }),
+});
+
 export const reminders = sqliteTable("reminders", {
-  id: int("id").primaryKey(),
-  authorId: int("author_id").notNull(),
+  id: int("id").primaryKey({ autoIncrement: true }),
+  authorId: int("author_id")
+    .notNull()
+    .references(() => users.id),
   text: text("text").notNull(),
+  time: text("time").notNull(),
 });
