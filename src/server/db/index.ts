828b21b537bef1821268e9f81333aaff12937b3a
Konstantin <konstantin.kim@flexsoft.uz>
2025-10-03 20:07:44 +0900

    feat: setup database connection using drizzle-orm and environment variables

---
+++ src/server/db/index.ts
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/libsql";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
+if (!process.env.DATABASE_AUTH_TOKEN) {
+  throw new Error("DATABASE_AUTH_TOKEN is not defined");
+}
+
+const db = drizzle({
+  connection: {
+    url: process.env.DATABASE_URL,
+    authToken: process.env.DATABASE_AUTH_TOKEN,
+  },
+});
