fcab0662193a6227280cd3bd1c9103c66696d060
Abdumalik <leko280208@gmail.com>
2025-11-11 16:21:46 +0900

    Trpc server was added to authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,29 +8,25 @@ import {
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import type React from "react";
 import TRPCProvider from "@/components/TRPCProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
 export const metadata: Metadata = {
   title: "Voice AI",
   description: "Personal AI Assistant",
 };
-
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <ClerkProvider>
       <html lang="en">
@@ -46,8 +42,6 @@ export default function RootLayout({
               <UserButton />
             </SignedIn>
           </header>
-
-          {/* Your app's content wrapped with TRPC provider */}
           <TRPCProvider>{children}</TRPCProvider>
         </body>
       </html>
