5044ee94cb9a6696b296c1029aedab86ad292cc1
Konstantin Kim <litepas@litepas.me>
2025-12-10 01:05:57 +0900

    feat: integrate `@serwist/next` for custom service worker support and add relevant type dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
 
+const withSerwist = require("@serwist/next").default({
+  swSrc: "src/service-worker/index.ts",
+  swDest: "public/sw.js",
+  // disable: process.env.NODE_ENV !== "production",
+});
+
 const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
   },
 };
 
-export default nextConfig;
+export default withSerwist(nextConfig);
