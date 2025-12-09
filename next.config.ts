7ea51f8b23f28b11ea7e19d0d22347a12854dd33
Konstantin Kim <litepas@litepas.me>
2025-12-10 02:12:00 +0900

    refactor: remove `@serwist/next` integration and enable `reactStrictMode` for Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,10 @@
 import type { NextConfig } from "next";
 
-const withSerwist = require("@serwist/next").default({
-  swSrc: "src/service-worker/index.ts",
-  swDest: "public/sw.js",
-  // disable: process.env.NODE_ENV !== "production",
-});
-
 const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
   },
+  reactStrictMode: true,
 };
 
-export default withSerwist(nextConfig);
+export default nextConfig;
